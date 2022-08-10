4da9e06f3537cbdac7ec73e3ff36020eaa5089c8
Vinicius Oliveira <vinicius.oliveira-ext@ab-inbev.com>
2022-08-10 14:30:13 -0300

    DB read|update|s3 copy working

--- deploy.py
+++ deploy.py
@@ -4,7 +4,6 @@ import psycopg2
 import logging
 import sys
 import argparse
-import bash
 
 
 # PSQL DB connection string
@@ -24,14 +23,6 @@ destination_bucket = 'vini-sketch-prod-s3'
 
 # Connect to db
 try:
-    #     conn = psycopg2.connect(DB_CONN_STRING)
-    # except Exception as e:
-    #     logging.error(f"Error while connecting to the database: {e}")
-    #     sys.exit(1)
-    #     cursor = connection.cursor()
-
-    # Create a Source Dictionary That Specifies Bucket Name and Key Name of the Object to Be Copied
-
     connection = psycopg2.connect(user="postgres",
                                   password="mysecretpassword",
                                   host="172.17.0.3",
@@ -44,7 +35,6 @@ try:
     print("Selecting AWS S3 paths from table avatars")
     s3_path = cursor.fetchall()
 
-    print("Print each row and it's columns values")
     for row in s3_path:
         copy_source = {
             'Bucket': source_bucket,
@@ -52,21 +42,43 @@ try:
         }
 
         bucket = s3.Bucket(destination_bucket)
+        new_row = row[1].split("/")[0]+"_prod" + "/"+row[1].split("/")[1]
+        bucket.copy(copy_source, new_row)
+        print(new_row)
+        print(row[1])
 
-        bucket.copy(copy_source, row[1])
+        print("Table Before updating record ")
+        sql_select_query = """select * from avatars where path = %s"""
+        cursor.execute(sql_select_query, (row[1],))
+        record = cursor.fetchone()
+        print(record)
+
+        # Update single record now
+        sql_update_query = """Update avatars set path = %s where path = %s"""
+        cursor.execute(sql_update_query, (new_row, row[1]))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Record Updated successfully ")
+
+        print("Table After updating record ")
+        sql_select_query = """select * from avatars where path = %s"""
+        cursor.execute(sql_select_query, (new_row,))
+        record = cursor.fetchone()
+        print(record)
+
+        # cursor.execute(
+        #     f'UPDATE avatars SET path = {new_row} WHERE path = {row[1]}')
+
+        # cursor.execute(
+        #     f'UPDATE avatars SET path = "{new_row}" WHERE path = "{row[1]}"')
+
+        # cursor.execute('UPDATE avatars SET path = ' +
+        #                new_row + ' WHERE id = ' + row[1])
+        # # cursor.execute(
+        # "UPDATE avatars SET path = " + new_row + " WHERE path = " + 'row[1]')
 
 except (Exception, psycopg2.Error) as error:
     print("Error while fetching data from PostgreSQL", error)
-
-
-# def insert_db_row(connection, path):
-#     try:
-#         cur = conn.cursor()
-#         cur.execute("INSERT INTO avatars (path) VALUES (%s)", (path,))
-#         conn.commit()
-#     except Exception as e:
-#         logging.error(f"Error inserting to the database: {e}")
-#         sys.exit(1)
 
 
 # # Creating S3 Resource From the Session.
