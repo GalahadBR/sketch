c70aad0cf20f4a19ad61d380d7a4071bd9ebd5a8
Vinicius Oliveira <vinicius.oliveira-ext@ab-inbev.com>
2022-08-10 18:08:33 -0300

    Adding upload images script

---
+++ upload_files.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+from glob import glob
+import boto3
+
+AWS_REGION = 'us-east-2'
+
+bucket_name = 'vini-sketch-legacy-s3'
+location = {'LocationConstraint': AWS_REGION}
+
+# Creating Session With Boto3.
+session = boto3.Session(
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
+client = boto3.client('s3', region_name=AWS_REGION)
+
+
+def upload_file(file_name, bucket, object_name=None, args=None):
+    if object_name is None:
+        object_name = file_name
+
+    client.upload_file(file_name, bucket, object_name, ExtraArgs=args)
+    print(f"'{file_name}' has been uploaded to '{bucket_name}'")
+
+
+files = glob(f'image/*.png')
+
+for file in files:
+    upload_file(file, bucket_name)
